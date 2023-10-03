90e45df2622d3f93b9b0deb85fc9fbca1c64a98a
Artur Luiz da Silva Oliveira <artur@arturluiz.com.br>
2023-10-03 14:15:32 -0300

    Adding basics

---
+++ docs/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    // Function to generate search criteria
+    function generateSearchCriteria() {
+        const type = $('#type').val();
+        const minCP = $('#minCP').val();
+        const maxCP = $('#maxCP').val();
+        
+        let criteria = '';
+        
+        if (type !== 'all') {
+            criteria += `type:${type} `;
+        }
+        
+        if (minCP !== '') {
+            criteria += `cp:${minCP}-${maxCP} `;
+        }
+        
+        $('#searchCriteria').val(criteria.trim());
+    }
+    
+    // Trigger the generation function whenever any form element changes
+    $('select, input').on('change', function() {
+        generateSearchCriteria();
+    });
+    
+    // Click event for the copy button
+    $('#copyButton').on('click', function() {
+        $('#searchCriteria').select();
+        document.execCommand('copy');
+        alert('Search criteria copied to clipboard!');
+    });
+});
